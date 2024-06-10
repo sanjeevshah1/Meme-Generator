74929ad326ac0c8659dbe4c78d8cab4f95eb16a0
sanjeevshah1 <sahsanjeev42@gmail.com>
2024-06-10 19:45:54 +0545

    modified css and Navigation

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -1,12 +1,9 @@
 import logo from "./../images/meme.png";
 export default function Navigation() {
   return (
-    <nav>
-      <div className="logo">
+    <nav className="logo"> 
         <img id="meme-logo" src={logo} alt="laughing" />
         <h1 id="title">Meme Generator</h1>
-      </div>
-      <h3 id="secondary-title">React Course - Project 3</h3>
     </nav>
   );
 }
